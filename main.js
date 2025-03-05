d5f0cbf8c98e3a2ee07784f4ece67d54aba667cd
Inno <enoatoz6@gmail.com>
2025-03-05 13:03:53 -0500

    adding classes

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-Navbaritem1 = document.querySelector(".navbar--item_1");
-h1 = document.querySelector(".h1");
-Navnavbar = document.querySelector(".Navnavbar");
+//Globals
+//Nav Bar
+class Navbar{
 
-Navbaritem1.addEventListener("mouseover", () => {
+Navbaritem1 = document.querySelector(".navbar--item_1");
+h1 = document.querySelector(".bottom--navbar");
+Navnavbar = document.querySelector(".Navbar");
+
+this.Navbaritem1.addEventListener("mouseover", () => {
   h1.style.display = "flex";
 });
 
 Navnavbar.addEventListener("mouseleave", () => {
   h1.style.display = "none";
 });
+}
+//Calling
