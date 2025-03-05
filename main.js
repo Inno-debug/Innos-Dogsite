99af11195a10b7053ed74cf8e747eea21ff34dab
Inno <enoatoz6@gmail.com>
2025-03-05 16:16:46 -0500

    fixing nav bar

--- main.js
+++ main.js
@@ -1,79 +1,78 @@
 //Globals
 //Nav Bar
-class Nav{
-constructor(){
-  this.Navbaritem1 = document.querySelector(".navbar--item_1");
-  this.Navbaritem2 = document.querySelector(".navbar--item_2");
-  this.Navbaritem3 = document.querySelector(".navbar--item_3");
-  this.Navbaritem4 = document.querySelector(".navbar--item_4");
-  this.Navbaritem5 = document.querySelector(".navbar--item_5");
-  this.B_navbar1 = document.querySelector(".bottom--navbar1");
-  this.B_navbar2 = document.querySelector(".bottom--navbar2");
-  this.B_navbar3 = document.querySelector(".bottom--navbar3");
-  this.B_navbar4 = document.querySelector(".bottom--navbar4");
-  this.B_navbar5 = document.querySelector(".bottom--navbar5");
-  this.Navbar = document.querySelector(".Navbar");
-  //Mini bar 1
-  this.Navbaritem1.addEventListener("mouseover", () => {
-    this.B_navbar1.style.display = "flex";
-    this.B_navbar2.style.display = "none";
-    this.B_navbar3.style.display = "none";
-    this.B_navbar4.style.display = "none";
-    this.B_navbar5.style.display = "none";
-  });
-  
-  this.Navbar.addEventListener("mouseleave", () => {
-    this.B_navbar1.style.display = "none";
-  });
-  //Mini bar 2
-  this.Navbaritem2.addEventListener("mouseover", () => {
-    this.B_navbar2.style.display = "flex";
-    this.B_navbar1.style.display = "none";
-    this.B_navbar3.style.display = "none";
-    this.B_navbar4.style.display = "none";
-    this.B_navbar5.style.display = "none";
-  });
-  
-  this.Navbar.addEventListener("mouseleave", () => {
-    this.B_navbar2.style.display = "none";
-  });
-//Mini bar 3
-  this.Navbaritem3.addEventListener("mouseover", () => {
-    this.B_navbar3.style.display = "flex";
-    this.B_navbar2.style.display = "none";
-    this.B_navbar1.style.display = "none";
-    this.B_navbar4.style.display = "none";
-    this.B_navbar5.style.display = "none";
-  });
-  
-  this.Navbar.addEventListener("mouseleave", () => {
-    this.B_navbar3.style.display = "none";
-  });
-//Mini bar 4
-  this.Navbaritem4.addEventListener("mouseover", () => {
-    this.B_navbar4.style.display = "flex";
-    this.B_navbar2.style.display = "none";
-    this.B_navbar3.style.display = "none";
-    this.B_navbar1.style.display = "none";
-    this.B_navbar5.style.display = "none";
-  });
-  
-  this.Navbar.addEventListener("mouseleave", () => {
-    this.B_navbar4.style.display = "none";
-  });
-//Mini bar 5
-  this.Navbaritem5.addEventListener("mouseover", () => {
-    this.B_navbar5.style.display = "flex";
-    this.B_navbar2.style.display = "none";
-    this.B_navbar3.style.display = "none";
-    this.B_navbar4.style.display = "none";
-    this.B_navbar1.style.display = "none";
-  });
-  
-  this.Navbar.addEventListener("mouseleave", () => {
-    this.B_navbar5.style.display = "none";
-  });
-}
+class Nav {
+  constructor() {
+    this.Navbaritem1();
+    this.Navbaritem2();
+    this.Navbaritem3();
+    this.Navbaritem4();
+    this.Navbaritem5();
+  }
+  //Navbar item 1
+  Navbaritem1() {
+    const navbaritem1 = document.querySelector(".navbar--item_1");
+
+    navbaritem1.addEventListener("mouseover", () => {
+      navbaritem1.style.filter = "var(--hover-textopacity)";
+      navbaritem1.style.cursor = "pointer";
+    });
+    navbaritem1.addEventListener("mouseout", () => {
+      navbaritem1.style.filter = "var(--nohover-textopacity)";
+      navbaritem1.style.cursor = "default";
+    });
+  }
+  //Navbar item 2
+  Navbaritem2() {
+    const navbaritem2 = document.querySelector(".navbar--item_2");
+
+    navbaritem2.addEventListener("mouseover", () => {
+      navbaritem2.style.filter = "var(--hover-textopacity)";
+      navbaritem2.style.cursor = "pointer";
+    });
+    navbaritem2.addEventListener("mouseout", () => {
+      navbaritem2.style.filter = "var(--nohover-textopacity)";
+      navbaritem2.style.cursor = "default";
+    });
+  }
+  //Navbar item 3
+  Navbaritem3() {
+    const navbaritem3 = document.querySelector(".navbar--item_3");
+
+    navbaritem3.addEventListener("mouseover", () => {
+      navbaritem3.style.filter = "var(--hover-textopacity)";
+      navbaritem3.style.cursor = "pointer";
+    });
+    navbaritem3.addEventListener("mouseout", () => {
+      navbaritem3.style.filter = "var(--nohover-textopacity)";
+      navbaritem3.style.cursor = "default";
+    });
+  }
+  //Navbar item 4
+  Navbaritem4() {
+    const navbaritem4 = document.querySelector(".navbar--item_4");
+
+    navbaritem4.addEventListener("mouseover", () => {
+      navbaritem4.style.filter = "var(--hover-textopacity)";
+      navbaritem4.style.cursor = "pointer";
+    });
+    navbaritem4.addEventListener("mouseout", () => {
+      navbaritem4.style.filter = "var(--nohover-textopacity)";
+      navbaritem4.style.cursor = "default";
+    });
+  }
+  //Navbar item 5
+  Navbaritem5() {
+    const navbaritem5 = document.querySelector(".navbar--item_5");
+
+    navbaritem5.addEventListener("mouseover", () => {
+      navbaritem5.style.filter = "var(--hover-textopacity)";
+      navbaritem5.style.cursor = "pointer";
+    });
+    navbaritem5.addEventListener("mouseout", () => {
+      navbaritem5.style.filter = "var(--nohover-textopacity)";
+      navbaritem5.style.cursor = "default";
+    });
+  }
 }
 //Calling
 new Nav();
