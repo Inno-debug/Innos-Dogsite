5a66cbd50e5464c1c5079414768e891fca75af3a
Inno <enoatoz6@gmail.com>
2025-03-07 14:42:24 -0500

    made full functional login screen

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 class initialScreen {
   constructor() {
-    this.initialLogin();
-    this.initialRegister();
+    this.initialLogin_Button();
+    this.initialRegister_Button();
   }
 
-  initialLogin() {
+  initialLogin_Button() {
     const loginButton = document.querySelector(".login_initial_screen");
     const initialScreen = document.querySelector(".initial-screen");
     const loginScreen = document.querySelector(".login-container");
@@ -14,7 +14,7 @@ class initialScreen {
       loginScreen.style.display = "unset";
     });
   }
-  initialRegister() {
+  initialRegister_Button() {
     const registerButton = document.querySelector(".register_initial_screen");
     const initialScreen = document.querySelector(".initial-screen");
     const registerScreen = document.querySelector(".register-container");
@@ -28,115 +28,27 @@ class initialScreen {
 
 class registerScreen {
   constructor() {
-    this.registerLogin();
+    this.registerLogin_Button();
     this.register();
   }
   register() {
     const form = document.querySelector(".registerform");
+    const registerScreen = document.querySelector(".register-container");
+    const loginScreen = document.querySelector(".login-container");
     form.addEventListener("submit", (event) => {
       event.preventDefault();
-      let user = localStorage.getItem("user");
-      user = user ? parseInt(user) : 0;
-      if (user === 0) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username", username);
-        localStorage.setItem("password", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 1) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username1", username);
-        localStorage.setItem("password1", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 2) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username2", username);
-        localStorage.setItem("password2", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 3) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username3", username);
-        localStorage.setItem("password3", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 4) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username4", username);
-        localStorage.setItem("password4", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 5) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username5", username);
-        localStorage.setItem("password5", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 6) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username6", username);
-        localStorage.setItem("password6", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 7) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username7", username);
-        localStorage.setItem("password7", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 8) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username8", username);
-        localStorage.setItem("password8", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 9) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username9", username);
-        localStorage.setItem("password9", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 10) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username10", username);
-        localStorage.setItem("password10", password);
-        ++user;
-        localStorage.setItem("user", user);
-      } else if (user === 11) {
-        const formData = new FormData(form);
-        const username = formData.get("username");
-        const password = formData.get("password");
-        localStorage.setItem("username11", username);
-        localStorage.setItem("password11", password);
-        ++user;
-        localStorage.setItem("user", user);
-      }
+      const formData = new FormData(form);
+      const username = formData.get("username");
+      const password = formData.get("password");
+      let formArray = localStorage.getItem("formArray");
+      formArray = formArray ? JSON.parse(formArray) : [];
+      formArray.push({ username, password });
+      localStorage.setItem("formArray", JSON.stringify(formArray));
+      registerScreen.style.display = "none";
+      loginScreen.style.display = "unset";
     });
   }
-  registerLogin() {
+  registerLogin_Button() {
     const loginButton = document.querySelector(".backloginButton");
     const registerScreen = document.querySelector(".register-container");
     const loginScreen = document.querySelector(".login-container");
@@ -149,9 +61,32 @@ class registerScreen {
 }
 class loginScreen {
   constructor() {
-    this.loginRegister();
+    this.loginRegister_Button();
+    this.login();
   }
-  loginRegister() {
+  login() {
+    const form = document.querySelector(".loginform");
+    const welcomeScreen = document.querySelector(".home-screen");
+    const loginScreen = document.querySelector(".login-container");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(form);
+      const username = formData.get("username");
+      const password = formData.get("password");
+      let formArray = localStorage.getItem("formArray");
+      formArray = JSON.parse(formArray);
+      const formarrayLength = formArray.length;
+      for (let i = 0; i < formarrayLength; i++) {
+        const storedusername = formArray[i].username;
+        const storedpassword = formArray[i].password;
+        if (storedusername === username && storedpassword === password) {
+          loginScreen.style.display = "none";
+          welcomeScreen.style.display = "unset";
+        }
+      }
+    });
+  }
+  loginRegister_Button() {
     const registerButton = document.querySelector(".backregisterButton");
     const registerScreen = document.querySelector(".register-container");
     const loginScreen = document.querySelector(".login-container");
