7c4170ef871d01288a97dd23241b8eb1708e6e4b
Inno <enoatoz6@gmail.com>
2025-03-06 21:03:36 -0500

    functional welcome screen

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-class mainscreen {
-  constructor() {}
+class initialScreen {
+  constructor() {
+    this.initialLogin();
+    this.initialRegister();
+  }
+
+  initialLogin() {
+    const loginButton = document.querySelector(".login_initial_screen");
+    const initialScreen = document.querySelector(".initial-screen");
+    const loginScreen = document.querySelector(".login-container");
+
+    loginButton.addEventListener("click", () => {
+      initialScreen.style.display = "none";
+      loginScreen.style.display = "unset";
+    });
+  }
+  initialRegister() {
+    const registerButton = document.querySelector(".register_initial_screen");
+    const initialScreen = document.querySelector(".initial-screen");
+    const registerScreen = document.querySelector(".register-container");
+
+    registerButton.addEventListener("click", () => {
+      initialScreen.style.display = "none";
+      registerScreen.style.display = "unset";
+    });
+  }
 }
 
-class register {
+class registerScreen {
   constructor() {}
 }
-class login {
+class loginScreen {
   constructor() {}
 }
-
+new initialScreen();
 new register();
+new login();
